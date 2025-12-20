c4ee6d073f2b36a9c40f61a971b1c4be658843b2
bala-amypo <bala@amypo.in>
2025-12-20 14:30:50 +0000

    User commit

--- src/main/java/com/example/demo/model/Emplyee.java
+++ src/main/java/com/example/demo/model/Emplyee.java
@@ -91,3 +91,38 @@
 //         return createdAt;
 //     }
 // }
+package com.example.demo.model;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+@Entity
+@Table(name="employees",uniqueConstraints=@UniqueConstraint(columnNames="email"))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable=false)
+    private String fullName;
+    @Column(nullable=false,unique=true)
+    private String email;
+    @Column(nullable=false)
+    private String role="STAFF"; 
+    @Column
+    private String skills; 
+    @Column(nullable=false)
+    private Integer maxWeeklyHours;
+    @Column(nullable=false,updatable=false)
+    private LocalDateTime createdAt=LocalDateTime.now();
+    public List<String> getSkillsList() {
+        if (skills == null||skills.isBlank()){
+            return List.of();
+        }
+        return Arrays.asList(skills.split(","));
+    }
+}

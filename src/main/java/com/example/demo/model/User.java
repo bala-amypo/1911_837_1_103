dda4848b0f7d965ee21a8ecae66653fb5ab1de96
bala-amypo <bala@amypo.in>
2025-12-20 05:20:58 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,63 +1,86 @@
+// package com.example.demo.model;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+// import jakarta.persistence.UniqueConstraint;
+// @Entity
+// @Table(name="Users",
+//     uniqueConstraints={@UniqueConstraint(columnNames="email")})
+// public class User {
+//      @Id
+//      @GeneratedValue(strategy=GenerationType.IDENTITY)
+//      private int id;
+//      @Column(nullable=false)
+//      private String name;
+//      @Column(nullable=false,unique=true)
+//      private String email;
+//      @Column(nullable=false)
+//      private String password;
+//      @Column(nullable=false)
+//      private String role="ANALYST";
+//      public int getId() {
+//          return id;
+//      }
+//      public void setId(int id) {
+//          this.id = id;
+//      }
+//      public String getName() {
+//          return name;
+//      }
+//      public void setName(String name) {
+//          this.name = name;
+//      }
+//      public String getEmail() {
+//          return email;
+//      }
+//      public void setEmail(String email) {
+//          this.email = email;
+//      }
+//      public String getPassword() {
+//          return password;
+//      }
+//      public void setPassword(String password) {
+//          this.password = password;
+//      }
+//      public User(int id, String name, String email, String password, String role) {
+//         this.id = id;
+//         this.name = name;
+//         this.email = email;
+//         this.password = password;
+//         this.role = role;
+//     }
+//      public String getRole() {
+//          return role;
+//      }
+//      public void setRole(String role) {
+//          this.role = role;
+//      }
+//      public User() {
+//      }
+// }
 package com.example.demo.model;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
+import lombok.*;
 @Entity
-@Table(name="Users",
-    uniqueConstraints={@UniqueConstraint(columnNames="email")})
-public class User {
-     @Id
-     @GeneratedValue(strategy=GenerationType.IDENTITY)
-     private int id;
-     @Column(nullable=false)
-     private String name;
-     @Column(nullable=false,unique=true)
-     private String email;
-     @Column(nullable=false)
-     private String password;
-     @Column(nullable=false)
-     private String role="ANALYST";
-     public int getId() {
-         return id;
-     }
-     public void setId(int id) {
-         this.id = id;
-     }
-     public String getName() {
-         return name;
-     }
-     public void setName(String name) {
-         this.name = name;
-     }
-     public String getEmail() {
-         return email;
-     }
-     public void setEmail(String email) {
-         this.email = email;
-     }
-     public String getPassword() {
-         return password;
-     }
-     public void setPassword(String password) {
-         this.password = password;
-     }
-     public User(int id, String name, String email, String password, String role) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-     public String getRole() {
-         return role;
-     }
-     public void setRole(String role) {
-         this.role = role;
-     }
-     public User() {
-     }
+@Table(name="users",uniqueConstraints=@UniqueConstraint(columnNames = "email"))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable=false)
+    private String name;
+    @Column(nullable=false, unique = true)
+    private String email;
+    @Column(nullable=false)
+    private String password;
+    @Column(nullable=false)
+    private String role="ANALYST";
 }
+

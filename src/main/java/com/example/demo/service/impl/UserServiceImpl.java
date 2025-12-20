4e8bca9f0e134b526f486dfa07150a289e5a94c7
bala-amypo <bala@amypo.in>
2025-12-20 06:38:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ResourceExistsException;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User register(User user){
+        if (userRepository.existsByEmail(user.getEmail())){
+            throw new ResourceExistsException("User with this email exists");
+        }
+        if (user.getRole() == null || user.getRole().isBlank()){
+            user.setRole("ANALYST");
+        }
+        return userRepository.save(user);
+    }
+    @Override
+    public User findByEmail(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(()->new ResourceNotFoundException("User not found"));
+    }
+}

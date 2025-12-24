d24b2d41520b0f6eb41e42d0b14859ad09c6839a
bala-amypo <bala@amypo.in>
2025-12-24 06:23:48 +0000

    User commit

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -1,20 +1,18 @@
 package com.example.demo.config;
 
-import com.example.demo.repository.UserRepository;
-import com.example.demo.service.UserService;
-import com.example.demo.service.impl.UserServiceImpl;
+import com.example.demo.repository.AvailabilityRepository;
+import com.example.demo.repository.EmployeeRepository;
+import com.example.demo.service.AvailabilityService;
+import com.example.demo.service.impl.AvailabilityServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebMvcConfig {
-@Bean
-public AvailabilityService availabilityService(AvailabilityRepository repo, EmployeeRepository empRepo) {
-    return new AvailabilityServiceImpl(repo, empRepo);
-}
 
     @Bean
-    public UserService userService(UserRepository userRepository) {
-        return new UserServiceImpl(userRepository);
+    public AvailabilityService availabilityService(AvailabilityRepository availabilityRepository,
+                                                   EmployeeRepository employeeRepository) {
+        return new AvailabilityServiceImpl(availabilityRepository, employeeRepository);
     }
 }

04ee8960cfe69d7c375044745880ce48a2c6fba5
bala-amypo <bala@amypo.in>
2025-12-24 05:26:48 +0000

    User commit

--- src/main/java/com/example/demo/exception/InvalidTimeOrderException.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.exception;
-public class InvalidTimeOrderException extends IllegalArgumentException{
-    public InvalidTimeOrderException(String message){
-        super(message);
-    }
-}
